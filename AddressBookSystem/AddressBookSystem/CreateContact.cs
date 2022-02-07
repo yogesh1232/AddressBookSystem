13aac083e3be8d63128078d337581208fabbaec7
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-02-07 20:17:11 +0530

    Contact is created

---
+++ AddressBookSystem/AddressBookSystem/CreateContact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class CreateContact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Zip { get; set; }
+        public double PhoneNumber { get; set; }
+        public string Email { get; set; }
+        /// <summary>
+        /// display contact information.
+        /// </summary>
+        public void displayContact()
+        {
+            Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName + " Address: " + this.Address +
+                                " City: " + this.City + " State: " + this.State + " Zip: " + this.Zip +
+                                  " PhoneNumber: " + this.PhoneNumber + " Email: " + this.Email);
+        }
+    }
+}
---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿using AddressBookSystem;
+
+Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+CreateContact program = new CreateContact()
+{
+    FirstName = Console.ReadLine(),
+    LastName = Console.ReadLine(),
+    Address = Console.ReadLine(),
+    City = Console.ReadLine(),
+    State = Console.ReadLine(),
+    Zip = Convert.ToDouble(Console.ReadLine()),
+    PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+    Email = Console.ReadLine(),
+};
+program.displayContact();
