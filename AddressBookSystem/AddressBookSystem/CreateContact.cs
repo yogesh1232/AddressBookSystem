9574822b96f03ed25c001bcb28a32e1a61eeb704
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-02-09 18:20:03 +0530

    Add a new contact to address booK

--- AddressBookSystem/AddressBookSystem/CreateContact.cs
+++ AddressBookSystem/AddressBookSystem/CreateContact.cs
@@ -25,5 +25,24 @@ namespace AddressBookSystem
                                 " City: " + this.City + " State: " + this.State + " Zip: " + this.Zip +
                                   " PhoneNumber: " + this.PhoneNumber + " Email: " + this.Email);
         }
+
+        public void AddContact()
+        {
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            CreateContact contactPerson = new CreateContact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            List<CreateContact> list = new List<CreateContact>();
+            list.Add(contactPerson);
+            contactPerson.displayContact();
+        }
     }
 }
